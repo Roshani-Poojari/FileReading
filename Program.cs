8c4274bafef0fabc8d78efeeb55836aafb38a879
Roshani-Poojari <roshanipoojari2002@gmail.com>
2024-07-22 19:18:34 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System.IO;
+
+namespace FileHandlingReadDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string file1 = @"C:\Users\roshani.poojari\Desktop\Training\FileHandlingReadDemo\assets\ReadFile.txt";
+            string file2 = @"C:\Users\roshani.poojari\Desktop\Training\FileHandlingReadDemo\assets\Trial.html";
+
+            Console.WriteLine("Reading Txt File");
+            if (File.Exists(file1))
+            {
+                using (StreamReader sr = new StreamReader(file1))
+                {
+                    Console.WriteLine(sr.ReadToEnd());
+                }
+            }
+            else
+                Console.WriteLine("File does not exist");
+            Console.WriteLine("Reading Html File");
+            if (File.Exists(file2))
+            {
+                using (StreamReader sr = new StreamReader(file2))
+                {
+                    Console.WriteLine(sr.ReadToEnd());
+                }
+            }
+            else
+                Console.WriteLine("File does not exist");
+        }
+    }
+}
+
+
